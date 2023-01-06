acce172162c5a60b2c94091397d558228957600b
Cedric Louyot <cedric.louyot@gmail.com>
2023-01-06 17:43:40 +0100

    Add models and controllers test

--- test/controllers/shops_controller_test.rb
+++ test/controllers/shops_controller_test.rb
@@ -1,7 +1,45 @@
-require "test_helper"
+require 'test_helper'
 
 class ShopsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should display shop list in default language' do
+    get '/shops'
+    assert_response :success
+    assert_select 'h1', 'Liste des magasins'
+    shops.each do |shop|
+      assert_select 'a', shop.name
+    end
+  end
+
+  test 'should display shop list in english' do
+    get '/en/shops'
+    assert_response :success
+    assert_select 'h1', "Shops' list"
+    shops.each do |shop|
+      assert_select 'a', shop.name
+    end
+  end
+
+  test 'Should display shop1 schedule in default language' do
+    shop1 = shops(:shop1)
+    travel_to Date.new(2023, 1, 4) do
+      # January the 4th of 2023 is a wednesday
+      get "/shops/#{shop1.id}"
+      assert_response :success
+      assert_select 'h1', shop1.name
+      assert_select 'tr:nth-child(1) th', 'Horaires :'
+      assert_select 'tr:nth-child(1) td', 'mercredi'
+      assert_select 'tr:nth-child(1) td', "09:00-12:00,\n            14:30-18:00"
+
+      assert_select 'tr:nth-child(2) th', ''
+      assert_select 'tr:nth-child(2) td', 'jeudi'
+      assert_select 'tr:nth-child(2) td', 'Fermé'
+
+      assert_select 'tr:nth-child(7) th', ''
+      assert_select 'tr:nth-child(7) td', 'mardi'
+      assert_select 'tr:nth-child(7) td', '06:00-12:00'
+
+      assert_select 'tr:nth-child(8) th', false
+      assert_select 'tr:nth-child(8) td', false
+    end
+  end
 end
--- test/models/opening_range_test.rb
+++ test/models/opening_range_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class OpeningRangeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "week_openings method" do
+    week_openings = shops(:shop1).week_openings
+    expected = [[], [], [[21_600, 43_200]], [[32_400, 43_200], [52_200, 64_800]], [], [], []]
+    assert_equal expected, week_openings
+  end
 end
